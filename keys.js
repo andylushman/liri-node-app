35fc576631f5e5c6f68eb77e262aab471bc683fe
Andy Lushman <andylushman@gmail.com>
2017-07-11 13:34:32 -0600

    updated keys.js with the appropriate info

---
+++ keys.js
@@ -0,0 +1,8 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: '2QUc6tbMGVVUm2QPyJFKJ8n0S',
+  consumer_secret: '8EAC7JcjITrwLAAoJvgpGhVwe6LxzGI2i3d2J1hAwZKfO79uIM',
+  access_token_key: '	884854706388049922-RoioH4YjI3Nol9O0ZsEtXwlneCXOXHy',
+  access_token_secret: '	waED4xGyWWECXMrHGosyGKWrUbNF6SXjpdtbNr2ZIIsiB',
+}
