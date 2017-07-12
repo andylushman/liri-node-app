e39af0ea0f5e7ef0c264c13d24597ae5d9f0f5ed
Andy Lushman <andylushman@gmail.com>
2017-07-12 12:07:06 -0600

    Created empty functions

--- keys.js
+++ keys.js
@@ -4,5 +4,5 @@ exports.twitterKeys = {
   consumer_key: '2QUc6tbMGVVUm2QPyJFKJ8n0S',
   consumer_secret: '8EAC7JcjITrwLAAoJvgpGhVwe6LxzGI2i3d2J1hAwZKfO79uIM',
   access_token_key: '	884854706388049922-RoioH4YjI3Nol9O0ZsEtXwlneCXOXHy',
-  access_token_secret: '	waED4xGyWWECXMrHGosyGKWrUbNF6SXjpdtbNr2ZIIsiB',
+  access_token_secret: 'waED4xGyWWECXMrHGosyGKWrUbNF6SXjpdtbNr2ZIIsiB'
 }
--- liri.js
+++ liri.js
@@ -1,34 +1,35 @@
 //=======================
 //GLOBAL VARIABLES
 //=======================
-
 //Load Required Node Modules
 const twitter = require('twitter');
 //npm install --save node-spotify-api??
 const Spotify = require('node-spotify-api');
 const request = require('request'); //needed for OMBD API
-console.log("Required Node Modules loaded");
 
 //Load User Twitter Keys
 const keys = require("./keys.js")
 const twitterKeys = keys.twitterKeys;
-console.log("keys loaded");
 
-const client = new Twitter(keys);
-
+//New twitter client
+const client = new Twitter(twitterKeys);
+const params = {screen_name: 'aldubootcamp'};
 
 //=======================
 //FUNCTIONS
 //=======================
+function retrieveTweets(){
 
+}
+
+function spotifySong(song){
+
+}
+
+function retrieveOBDBInfo(movie){
+
+}
 
 //=======================
 //MAIN PROCESS
 //=======================
-
-var params = {screen_name: 'aldubootcamp'};
-client.get('statuses/user_timeline', params, function(error, tweets, response) {
-  if (!error) {
-    console.log(tweets);
-  }
-});
