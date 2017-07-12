34c5e128300bf596fd9686bc82b8b84ed7a3919e
Andy Lushman <andylushman@gmail.com>
2017-07-12 11:33:32 -0600

    added twitter keys

--- liri.js
+++ liri.js
@@ -1,7 +1,4 @@
-/*write the code you need to grab the data from keys.js. Then store the keys in a variable.
-Make it so liri.js can take in one of the following commands:
+//Load User Twitter Keys
 
-my-tweets
-spotify-this-song
-movie-this
-do-what-it-says */
+const keys = require("./keys.js")
+const twitterKeys = liri.twitterKeys;
