2e97705f5469650f800dcb291bdcc470bb79b3f3
Andy Lushman <andylushman@gmail.com>
2017-07-12 11:46:10 -0600

    Loaded required node modules

--- liri.js
+++ liri.js
@@ -1,4 +1,11 @@
-//Load User Twitter Keys
+//Load Required Node Modules
+const Twitter = require('twitter');
+//npm install --save node-spotify-api??
+const Spotify = require('node-spotify-api');
+const request = require('request'); //needed for OMBD API
+console.log("Required Node Modules loaded");
 
+//Load User Twitter Keys
 const keys = require("./keys.js")
-const twitterKeys = liri.twitterKeys;
+const twitterKeys = keys.twitterKeys;
+console.log("keys loaded");
