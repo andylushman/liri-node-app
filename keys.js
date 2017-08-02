3d899321d0d8e7a47ab68f3bf00849093ee4e342
Andy Lushman <andylushman@gmail.com>
2017-08-01 18:40:50 -0600

    moved keys.js out of .gitignore

---
+++ keys.js
@@ -0,0 +1,12 @@
+
+exports.twitterKeys = {
+  consumer_key: '0ZzUyZcm1PXgGTYwhQDld56i2',
+  consumer_secret:"zFRqTRTEmY5oz2KAtU1xhGSnvwCHPVBzEFk1MPmTZZlaFRDkv5",
+  access_token_key: '884854706388049922-N66y0FD5ZNstrf4S6UgYpEKIEb4a0xH',
+  access_token_secret: 'vKRACWt8ye3mX2VMWJL5nvmC6h6zr5Ng383XSOOn4tAoU'
+}
+
+exports.spotifyKeys = {
+  id: "b4461ab30f4649e1918b475bcb73b586",
+  secret: "1020a7a916204915812f89386d37b290"
+}
